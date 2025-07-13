ffebfd84f9880d9043071581d72f14aebd12b5a5
shota-kita <shotakita11@gmail.com>
2025-07-13 15:12:48 +0900

    cssファイルの追加

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,7 +1,1 @@
 class Item < ApplicationRecord
-  has_one_attached :image
-
-  def item_params
-    params.require(:item).permit(:content, :image).merge(user_id: current_user.id)
-  end
-end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2025_07_08_081013) do
+ActiveRecord::Schema[7.1].define(version: 2025_07_08_081255) do
   create_table "active_storage_attachments", charset: "utf8mb3", force: :cascade do |t|
     t.string "name", null: false
     t.string "record_type", null: false
@@ -37,6 +37,11 @@ ActiveRecord::Schema[7.1].define(version: 2025_07_08_081013) do
     t.bigint "blob_id", null: false
     t.string "variation_digest", null: false
     t.index ["blob_id", "variation_digest"], name: "index_active_storage_variant_records_uniqueness", unique: true
+  end
+
+  create_table "items", charset: "utf8mb3", force: :cascade do |t|
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
