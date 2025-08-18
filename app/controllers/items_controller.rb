16674dd6ea5b6613d66ce9239c49f735663b2453
shota-kita <shotakita11@gmail.com>
2025-08-18 21:07:26 +0900

    不適切な記述の修正

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,5 +1,5 @@
 class ItemsController < ApplicationController
-  before_action :authenticate_user!, only: [:new, :create, :edit, :update]
+  before_action :authenticate_user!, only: [:new, :create, :edit, :update, :destroy]
   before_action :item_find, only: [:show, :edit, :update, :destroy]
   def index
     @items = Item.all.order('created_at DESC')
@@ -39,8 +39,7 @@ class ItemsController < ApplicationController
     return unless user_signed_in?
 
     if current_user.id == @item.user_id
-      item = Item.find(params[:id])
-      item.destroy
+      @item.destroy
       redirect_to_root
     else
       redirect_to_root
